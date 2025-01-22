798f5ea291b9e3ce6af42be3159d21e1fab63e2d
ayush0815 <predatorof09@gmail.com>
2025-01-22 14:39:08 +0530

    Level 1 Practice Problem

---
+++ ProfitAndLoss.java
@@ -0,0 +1,8 @@
+public class ProfitAndLoss {
+    public static void main(String[] args) {
+        double costPrice = 129, sellingPrice = 191;
+        double profit = sellingPrice - costPrice;
+        double profitPercentage = (profit / costPrice) * 100;
+        System.out.println("The Cost Price is INR " + costPrice + " and Selling Price is INR " + sellingPrice);
+        System.out.println("The Profit is INR " + profit + " and the Profit Percentage is " + profitPercentage);
+    }}
