b29e34926d09dac7efbf24b17a4a5c351c17e74c
ayush0815 <predatorof09@gmail.com>
2025-01-22 14:51:32 +0530

    Level 1 Practice Problem

---
+++ Calculator.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Calculator {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double number1 = sc.nextDouble();
+        double number2 = sc.nextDouble();
+        System.out.println("The addition, subtraction, multiplication and division value of 2 numbers " + number1 + " and " + number2 + " is " 
+                            + (number1 + number2) + ", " + (number1 - number2) + ", " + (number1 * number2) + ", and " + (number1 / number2));
+    }
+}
