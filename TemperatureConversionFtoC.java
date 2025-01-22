9e458b8ce0eb115051edf901f7bd319d42171274
ayush0815 <predatorof09@gmail.com>
2025-01-22 14:21:26 +0530

    Level 2 Practice Programs

---
+++ TemperatureConversionFtoC.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+class TemperatureConversionFtoC {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter the temperature in Fahrenheit: ");
+        double fahrenheit = input.nextDouble();
+
+        double celsius = (fahrenheit - 32) * 5 / 9;
+
+        System.out.println("The " + fahrenheit + " fahrenheit is " + celsius + " celsius.");
+    }
+}
