ff874416ae4022d2d22aacd66f3b4b5037d6dc48
ayush0815 <predatorof09@gmail.com>
2025-01-31 14:20:36 +0530

    Practice Problem

---
+++ Day6/Employee.java
@@ -0,0 +1,22 @@
+class Employee {
+    private String name;
+    private int id;
+    private double salary;
+
+    public Employee(String name, int id, double salary) {
+        this.name = name;
+        this.id = id;
+        this.salary = salary;
+    }
+
+    public void displayDetails() {
+        System.out.println("Name: " + name);
+        System.out.println("ID: " + id);
+        System.out.println("Salary: " + salary);
+    }
+
+    public static void main(String[] args) {
+        Employee emp1 = new Employee("John Doe", 12345, 60000);
+        emp1.displayDetails();
+    }
+}
