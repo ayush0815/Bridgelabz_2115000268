c5c1cbba53addfbaa6b3c822c6a93cb21e715ba0
ayush0815 <predatorof09@gmail.com>
2025-02-04 11:51:31 +0530

    Modified file

---
+++ Day7/Students.java
@@ -0,0 +1,47 @@
+class Student {
+    public int rollNumber;
+    protected String name;
+    private double CGPA;
+
+    public Student(int rollNumber, String name, double CGPA) {
+        this.rollNumber = rollNumber;
+        this.name = name;
+        this.CGPA = CGPA;
+    }
+
+    public double getCGPA() {
+        return CGPA;
+    }
+
+    public void setCGPA(double CGPA) {
+        this.CGPA = CGPA;
+    }
+}
+
+class PostgraduateStudent extends Student {
+    public String researchTopic;
+
+    public PostgraduateStudent(int rollNumber, String name, double CGPA, String researchTopic) {
+        super(rollNumber, name, CGPA);
+        this.researchTopic = researchTopic;
+    }
+
+    public void displayDetails() {
+        System.out.println("Roll Number: " + rollNumber);
+        System.out.println("Name: " + name);
+        System.out.println("CGPA: " + getCGPA());
+        System.out.println("Research Topic: " + researchTopic);
+    }
+}
+
+public class Students {
+    public static void main(String[] args) {
+        PostgraduateStudent student = new PostgraduateStudent(101, "Vaibhav", 8.3, "Blockchain");
+        student.displayDetails();
+        student.setCGPA(3.9);
+        System.out.println("Updated CGPA: " + student.getCGPA());
+    }
+}
+
+
+
